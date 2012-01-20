984696dc353128dd2df9de083a09f31718106936
Web Developer <webmaster@occasionsonline.com>
2012-01-20 17:43:28 -0500

    updated copy

--- wp-content/themes/oo1/vendor_access/terms.php
+++ wp-content/themes/oo1/vendor_access/terms.php
@@ -19,23 +19,18 @@
 <body> 
 <div class="post">
 <h1>Occasions Magazine, Inc. Advertising Contract Terms and Conditions</h1>
-<p>Advertiser acknowledges that this is a binding contract with Occasions Magazine, Inc. which shall be effective as of the date set forth above and shall continue based on the account type specified.   </p>
-<p>PLEASE CAREFULLY READ THE FOLLOWING TERMS BEFORE ACCESSING THIS SITE AND THE ADVERTISER CONTROL PANEL. BY ACCESSING THE SITE (WHICH INCLUDES THE ONLINE TRAINING LIBRARY®), YOU ACCEPT AND AGREE TO ALL OF THE COVENANTS AND CONDITIONS IMPOSED IN THIS AGREEMENT. IF YOU DO NOT AGREE TO THESE TERMS, YOU MAY NOT ACCESS THE SITE. </p>
-<p>BY USING OCCASIONS MAGAZINE, INC.'S SERVICE, YOU AGREE THAT YOU HAVE READ, UNDERSTAND AND AGREE TO THESE TERMS.  </p>
-
-<p><b>CONTACT INFORMATION</b></p>
-
-<p><b>Occasions Magazine, Inc.</b><br />
-2030 Avalon Parkway<br />
-Suite 405<br />
-McDonough, GA 30253<br /> 
-Phone: 888-262-8881<br />
-Email: info@occasionsonline.com</p>
-
-<p><b>Fees, renewal and refund policies</b></p>
-
-<p>Month-to-Month accounts include:</p>
-
+<p>Advertiser acknowledges that this is a binding contract with Occasions Magazine, Inc. which shall be effective as of the date set forth above and shall continue based on the account type specified.</p>
+<p>PLEASE CAREFULLY READ THE FOLLOWING TERMS BEFORE ACCESSING THIS SITE AND THE ADVERTISER CONTROL PANEL. BY ACCESSING THE SITE (WHICH INCLUDES THE ONLINE TRAINING LIBRARY®), YOU ACCEPT AND AGREE TO ALL OF THE COVENANTS AND CONDITIONS IMPOSED IN THIS AGREEMENT. IF YOU DO NOT AGREE TO THESE TERMS, YOU MAY NOT ACCESS THE SITE.</p>
+<p>BY USING OCCASIONS MAGAZINE, INC.'S SERVICE, YOU AGREE THAT YOU HAVE READ, UNDERSTAND AND AGREE TO THESE TERMS.</p>
+<p><strong>CONTACT INFORMATION</strong></p>
+Occasions Magazine, Inc.<br />
+2030 Avalon Parkway <br />
+Suite 405 <br />
+McDonough, GA 30253 <br />
+Phone: 888-262-8881 <br />
+Email: <a href="mailto:info@occasionsonline.com">info@occasionsonline.com</a><br />
+<p><strong>Fees, renewal and refund policies</strong></p>
+<p><strong>Month-to-Month accounts include:</strong></p>
 <ul>
 <li>Access to business profile features</li>
 <li>Monthly subscription plan</li>
@@ -45,12 +40,10 @@ Email: info@occasionsonline.com</p>
 <li>Subscription fees are non-refundable</li>
 <li>Advertiser may cancel account at anytime</li>
 <li>Occasions Magazine, Inc. may increase subscription fees for a subsequent subscription period at any time and for any reason, provided, however, that Occasions Magazine, Inc. provides notice at least thirty (30) OccasionsOnline.com may terminate the subscription and these Terms if unable to renew the subscription based on inaccurate or outdated credit card information</li>
-<li>Right of Access to the OccasionsOnline.com advertiser control panel granted under these Terms is effective only upon payment of the subscription fees </li>
+<li>Right of Access to the OccasionsOnline.com advertiser control panel granted under these Terms is effective only upon payment of the subscription fees</li>
 </ul>
 
-
-<p>Annual accounts include:</p>
-
+<p><strong>Annual accounts include:</strong></p>
 <ul>
 <li>Access to business profile features</li>
 <li>Annual subscription plan (12 month commitment)</li>
@@ -58,14 +51,19 @@ Email: info@occasionsonline.com</p>
 <li>Automatically renews on the same date of each corresponding year or on final date of month if no corresponding date (February 29, 2008 subscription renews on February 28, 2009)</li>
 <li>Fees charged on renewal date</li>
 <li>Subscription fees are non-refundable</li>
-<li>Advertise may cancel account at the end of their 12 month agreement. </li>
+<li>Advertise may cancel account at the end of their 12 month agreement.</li>
 <li>Occasions Magazine, Inc. may increase subscription fees for a subsequent subscription period at any time and for any reason, provided, however, that Occasions Magazine, Inc. provides notice at least thirty (30) calendar days prior to the expiration of the subscription</li>
 <li>Occasions Magazine, Inc. may terminate the subscription and these Terms if unable to renew the subscription based on inaccurate or outdated credit card information</li>
-<li>Right of Access to the OccasionsOnline.com advertiser control panel granted under these Terms is effective only upon payment of the subscription fees </li>
+<li>Right of Access to the OccasionsOnline.com advertiser control panel granted under these Terms is effective only upon payment of the subscription fees</li>
 </ul>
- 
-<p>All advertising matters shall be subject to approval by Occasions Magazine, Inc. prior to consent for advertising space. Occasions Magazine, Inc. reserves the right to refuse copy at its sole discretion. Occasions Magazine, Inc. does not guarantee ad placement/location. Advertiser represents Occasions Magazine, Inc. is authorized to publish the contents and subject matter of the advertisements covered by this contract, and that Advertiser possesses all necessary releases, consents, licenses, copyrights or trademarks regarding the content of the ad .  </p>
-<p>Advertiser agrees to indemnify Occasions Magazine, Inc. against any loss of expense resulting from any claims, actions or proceedings based on the contents or subject of such advertisements submitted, printed,and published.  </p>
+
+<p>Advertiser agrees to pay Occasions Magazine, Inc. the total advertising fees set forth above.  Payments are due on the date stated on the contract or invoice, or otherwise stated in the payment terms section of the advertising contract.  If any amounts owed to Occasions Magazine, Inc. under this agreement (including collection costs) are not paid when due, all payments, including those for services set forth on the advertising contract that have not yet been provided, may at Occasions Magazine, Inc.’s option, be accelerated and become immediately due and owing to Occasions Magazine, Inc..  Occasions Magazine, Inc. may remove the advertisement or service until such amounts are paid.  Any sums not paid when due shall bear interest starting 30 days after the invoice date at the rate of 1.5% per month (or, if lower, the highest rate allowed by applicable law), until such amounts are paid in full.  Deposits are non-refundable.  Advertiser Agreement automatically renews on anniversary date of contract.  Notice of termination or cancellation of advertising shall be given to Publisher, in writing, at least (30) days prior to the anniversary date. </p>
+
+<p>All advertising matters shall be subject to approval by Occasions Magazine, Inc. prior to consent for advertising space. Occasions Magazine, Inc. reserves the right to refuse copy at its sole discretion. Occasions Magazine, Inc. does not guarantee ad placement/location. Advertiser represents Occasions Magazine, Inc. is authorized to publish the contents and subject matter of the advertisements covered by this contract, and that Advertiser possesses all necessary releases, consents, licenses, copyrights or trademarks regarding the content of the ad.</p>
+<p>This Agreement and the rights and obligations hereunder are personal to Advertiser and may not be assigned by any act of Advertiser or by operation of law, change of control of Advertiser or otherwise without the prior written consent of Occasions Magazine, Inc., to granted or not granted in Occasions magazine, Inc.’s sole and absolute discretion.  Advertiser may not assign to, nor utilize for the benefit of another person or entity, any of the lineage required to be purchased by Advertiser without Occasions Magazine, Inc.’s prior written to consent, to be granted or not granted in Occasions Magazine, Inc.’s sole and absolute discretion.</p>
+<p>Any and all taxes levied against advertising shall be added to the advertising charges, including but not limited to any sales taxes.</p>
+<p>Occasions Magazine, Inc. shall have the right to use any advertising published or distributed in Occasions Magazine, Inc.’s publication for the purpose of promoting any of the products and services of Occasions Magazine, Inc. Advertiser grants Occasions Magazine, Inc. a non-exclusive, perpetual, irrevocable and worldwide license to publish any and all advertising content created by Advertiser or its agents or Occasions Magazine, Inc., including but not limited to photographs, artwork, text and graphics, in any media, presently known or unknown, including but not limited to Publisher’s electronic publications on the Internet and in any archival retrieval system whether that information is digitally stored or stored on any other media.</p>
+<p>Advertiser agrees to indemnify Occasions Magazine, Inc. against any loss of expense resulting from any claims, actions or proceedings based on the contents or subject of such advertisements submitted, printed, and published.</p>
 <p>Occasions Magazine, Inc. reserves the right to terminate this contract at any time upon default by Advertiser in the payment of bills or in the event of any other substantial breach of this contract by Advertiser. Upon such termination, all charges payable under this contract shall become immediately due and payable.</p>
 <p>This contract shall be governed by, and construed and enforced, under the laws of the State of Georgia and venue of any legal proceedings shall be in Henry County, Georgia. If any legal action or other proceeding, is brought for the enforcement of, or a dispute, breach, default or misrepresentation in connection with any provision of this contract, the successful or prevailing party shall be entitled to recover reasonable attorney's fees, court costs, and other reasonable expenses incurred in connection with maintaining or defending such action or proceeding, as the case might be, in addition to any other relief to which such party may be entitled.</p>
 <p>Occasions Magazine, Inc. will not be bound by any promises or agreements not contained herein, nor shall any such agreements relieve Advertiser or its agents (if any) of their obligation herein.</p>
